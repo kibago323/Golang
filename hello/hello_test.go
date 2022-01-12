d6e5616cd7c24a8344f51ee19851577dde46a698
Kaise Alnatour <kaise.alnatour@heronsystems.com>
2022-01-12 11:08:04 -0500

    Finished hello word with TDD

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,19 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("World!")
-	want := "Hello, World!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
-}
 
-func TestHelloTwo(t *testing.T) {
-	got := Hello("Chris!")
-	want := "Hello, Chris!"
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	t.Run(("say 'Hello, World' when an empty string is passed"), func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
