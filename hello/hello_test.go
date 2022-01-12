023c73203ac6717cfc7ba44ada14029730eb72ef
Kaise Alnatour <kaise.alnatour@heronsystems.com>
2022-01-12 09:34:13 -0500

    initial commit

---
+++ hello/hello_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello("World!")
+	want := "Hello, World!"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func TestHelloTwo(t *testing.T) {
+	got := Hello("Chris!")
+	want := "Hello, Chris!"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
