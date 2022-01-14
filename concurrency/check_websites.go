0548df18bc4c0b21d12fc46784a9bc5e34c0aede
Kaise Alnatour <kaise.alnatour@heronsystems.com>
2022-01-14 10:30:27 -0500

    I concur that concurrency is quite concurrent

---
+++ concurrency/check_websites.go
@@ -0,0 +1,39 @@
+package concurrency
+
+type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
+
+//Concurrent
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	resultChannel := make(chan result)
+
+	for _, url := range urls {
+		go func(u string) {
+			// Send statement
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		// Receive expression
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+
+	return results
+}
+
+// Sequential
+// func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+// 	results := make(map[string]bool)
+
+// 	for _, url := range urls {
+// 		results[url] = wc(url)
+// 	}
+
+// 	return results
+// }
